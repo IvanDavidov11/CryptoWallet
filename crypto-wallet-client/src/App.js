90ec5e81d600dde27db497aefbff3a1717cbd927
Ivan Davidov <andictus@gmail.com>
2024-10-12 16:34:07 +0300

    Initial commit and set-up for front end

---
+++ crypto-wallet-client/src/App.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const API_Url = "http://localhost:5033/api"
+
+  return (
+    <div className="App">
+          </div>
+  );
+}
+
+export default App;

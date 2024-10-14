9aecc922e9b0a0718942d5d9b35bedb7df25c28c
Ivan Davidov <andictus@gmail.com>
2024-10-14 22:03:33 +0300

    Created simple way to display initial value of portfolio

---
+++ crypto-wallet-client/src/LoadedPortfolio/InitialPortfolioValue.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const InitialPortfolioValue = () => {
+    const calculateInitial_ApiUrl="https://localhost:7038/api/calc/initial";
+    const [initalValue, setInitialValue] = useState(0);
+
+    useEffect (() => {
+        const fetchInitalValue = async () => {
+            try {
+              const response = await fetch(calculateInitial_ApiUrl);
+    
+              if (!response.ok) throw Error('Did not receive expected data');
+    
+              const initialValueInText = await response.json();
+              setInitialValue(parseFloat(initialValueInText));
+            }
+            catch (err) {
+              console.log(err);
+            }
+          }
+    
+          fetchInitalValue();
+    },[]);
+
+
+    return (
+        <div>
+            <h3>Initial Value:</h3>
+            <h4>{initalValue}</h4>
+        </div>
+    )
+}
+
+export default InitialPortfolioValue
--- crypto-wallet-client/src/LoadedPortfolio/LoadedPortfolio.js
+++ crypto-wallet-client/src/LoadedPortfolio/LoadedPortfolio.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CoinPortfolio from './CoinPortfolio/CoinPortfolio'
 import ClearPortfolio from './ClearPortfolio'
+import InitialPortfolioValue from './InitialPortfolioValue'
 
 const LoadedPortfolio = ({ coins, setFileUploaded}) => {
     return (
         <div className='loadedPortfolio'>
             <ClearPortfolio setFileUploaded={setFileUploaded}/>
             <CoinPortfolio coins={coins} />
+            <InitialPortfolioValue />
         </div>
     )
 }

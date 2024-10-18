5bb6d95b12d22ef246448d29142d5ae46141dd8b
Ivan Davidov <andictus@gmail.com>
2024-10-18 13:59:09 +0300

    Updated foramtting of prices to max 6 digits after decimal point.

--- crypto-wallet-client/src/CoinsTable/CoinRow.js
+++ crypto-wallet-client/src/CoinsTable/CoinRow.js
@@ -3,8 +3,8 @@ import React from 'react'
 const CoinRow = ({ coin, index }) => {
     const valueClass = coin.percentageChange?.toString().includes('-') ? 'value-negative' : 'value-positive';
 
-    const formattedBuyPrice = coin.buyPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
-    const formattedCurrentPrice = coin.currentPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+    const formattedBuyPrice = coin.buyPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 6 });
+    const formattedCurrentPrice = coin.currentPrice.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 6 });
 
     return (
         <tr className="coinTableRow">
